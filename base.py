e66ba6f1de46f12cf118ddd2acb25d7350a2e378
Your Name <tt7.thiago@gmail.com>
2020-03-26 18:44:21 -0300

    Atributos nascimento e telefone adicionados

--- base.py
+++ base.py
@@ -12,8 +12,13 @@ class Clientes():
          self.name = str(input("Digite o nome do cliente: "))
          self.endereco = str(input("Digite o endereço do referido cliente: "))
          self.idade = int(input("Digite a idade do cliente: "))
-         print("Cliente: "+ self.name)
+         self.nascimento = input("Digite a data de nascimento: ")
+         self.telefone = input("Digite a data de nascimento: ")
+         print("Cliente "+ self.name + "cadastrado com sucesso: ")
 
+#criar objeto csv e checar interdependências
+
+# criar um log dos cadastros que serão importados para o banco de dados.
 
 def desejodecadastro():
     print("Deseja Cadastrar um novo cliente: ")
@@ -29,17 +34,16 @@ def desejodecadastro():
         return desejodecadastro()
 
 def write_csv():
-    csv_header = ["Nome","Endereço", "Idade"]
+    csv_header = ["Nome","Endereço", "Idade","Nascimento" ,"Telefone"]
     csv_file = open("Clientes Cadastrados.csv","w",newline="")
     writer = csv.writer(csv_file,delimiter=";")
     writer.writerow(csv_header)
     writer.writerows(new_csv)
     print (new_csv)
 
-
 while desejodecadastro():
         cliente = Clientes()
-        csv_row =[cliente.name,cliente.endereco,cliente.idade]
+        csv_row =[cliente.name,cliente.endereco,cliente.idade , cliente.nascimento,cliente.telefone]
         new_csv.append(csv_row)
 
 write_csv()
